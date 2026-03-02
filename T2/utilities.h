83e641579ab7d892a721fcd80450aa724c1d0e2f
zzz271828 <lzha0272@student.monash.edu>
2026-03-02 19:42:07 +1100

    feat: accoding to the feedback

--- T2/utilities.h
+++ T2/utilities.h
@@ -10,28 +10,12 @@ These are the promises that these functions will exist and can be used.
 using std::string;
 
 
-/**
- * Read a string from the user
- *
- * @param prompt the message to show the user
- * @returns the string entered
- */
 string read_string(string prompt);
 
-/**
- * Read an integer from the user
- *
- * @param prompt the message to show the user
- * @returns the integer entered
- */
+
 int read_integer(string prompt);
 
-/**
- * Read a double from the user
- * 
- * @param prompt the message to show the user
- * @returns the double entered
-*/
+
 double read_double(string prompt);
 
 #endif
